e51575dbc6c902f542384ad791a33256c6c3cbbd
Alejandro <alextek1885@hotmail.com>
2020-08-31 18:21:33 +0100

    touch events in cards fixed for mobile view

--- js/script.js
+++ js/script.js
@@ -45,13 +45,50 @@ toggler.addEventListener('click', () => {
 window.addEventListener('scroll', () => {
     const offset = 110;
     const viewPortInnerHeight = window.innerHeight - offset;
-    console.log(viewPortInnerHeight)
+    // console.log(viewPortInnerHeight)
     if (window.scrollY > viewPortInnerHeight) {
         navbar.classList.add('colored');
-        console.log('color added')
+        // console.log('color added')
     } else {
         navbar.classList.remove('colored');
-        console.log('color removed')
+        // console.log('color removed')
 
     }
+});
+
+////////////////////////////
+////// flip card to original position
+const cardsBack = document.querySelectorAll('.card__side--back');
+const cardsFront = document.querySelectorAll('.card__side--front');
+
+
+const nonFlipped = () => {
+        const card = event.currentTarget.parentElement;
+        // console.log(card)
+        const front = card.children[0];
+        const back = card.children[1];
+        back.style.transform = 'rotateY(180deg)';
+        front.style.transform = 'rotateY(0)';
+        // console.log('touched end , nonFlipped()');
+}
+
+
+cardsBack.forEach(el => {
+    // el.addEventListener('touchend', nonFlipped )
+    el.addEventListener('click', nonFlipped )
+});
+
+const flipped = () => {
+        const card = event.currentTarget.parentElement;
+        // console.log(card)
+        const front = card.children[0];
+        const back = card.children[1];
+        back.style.transform = 'rotateY(0deg)';
+        front.style.transform = 'rotateY(-180deg)';
+        // console.log('touched end , flipped()');
+}
+
+
+cardsFront.forEach(el => {
+    el.addEventListener('touchend', flipped )
 });
